77f6b333bfaaab58160e0238c166dbe773966468
juasan <juasan@a1415>
2018-01-16 18:21:28 +0100

    temporal

---
+++ AceptaElReto1/src/aceptaelreto2/TendenciaLadoOscuro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aceptaelreto2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juasan
+ */
+public class TendenciaLadoOscuro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner b=new Scanner(System.in);
+        int dato=b.nextInt();
+        String numeros[]=new String[dato+1];
+     /*   
+        for(int i=0;i<=dato;i++)
+        {
+            numeros[i]=b.nextLine();
+            
+        }
+     */ 
+            
+            String base5=base(dato, 5);//test
+            System.out.println(base5);
+        
+    }
+    static String base(int num, int b) //convertir a base 5
+    {
+        if(num==0)
+            return "";
+        else
+            return base((num/b), b) + (num%b);
+    }
+    
+}
