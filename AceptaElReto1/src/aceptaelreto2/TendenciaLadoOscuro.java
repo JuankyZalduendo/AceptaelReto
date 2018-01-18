3b266a2bc41aa1c4a83afea0497aa391da8289b8
juan <>
2018-01-18 17:01:09 +0100

    Funcionando.

--- AceptaElReto1/src/aceptaelreto2/TendenciaLadoOscuro.java
+++ AceptaElReto1/src/aceptaelreto2/TendenciaLadoOscuro.java
@@ -18,19 +18,16 @@ public class TendenciaLadoOscuro {
      */
     public static void main(String[] args) {
         Scanner b=new Scanner(System.in);
-        int dato=b.nextInt();
-        String guardar[]=new String[dato+1];
-        
+        int dato=b.nextInt();    
         for(int i=0;i<dato;i++)
         {
             int temp=b.nextInt();
             String base5=base(temp, 5);
-            guardar[i]=base5;
-            
-           // System.out.println(base5);
-        }
-        
-        
+            if(cuantos4(base5)>1)
+                System.out.println("SI");
+            else
+                System.out.println("NO");
+        }      
     }
     static String base(int num, int b) //convertir a base 5
     {
@@ -38,6 +35,22 @@ public class TendenciaLadoOscuro {
             return "";
         else
             return base((num/b), b) + (num%b);
+    }
+    
+    static int cuantos4(String numero)
+    {
+        char temp[]=numero.toCharArray();
+        int cantidad=0;
+        for(int i=0;i<temp.length;i++)
+        {
+            if(temp[i]=='4')
+                cantidad++;
+        }
+        
+        if(cantidad>1)
+            return cantidad;
+        else
+            return 1;
     }
     
 }
