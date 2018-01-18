de1198760bcadaf230ca3b2f3feaac18f55e6d5d
JuanCZalduendo <32577738+JuankyZalduendo@users.noreply.github.com>
2018-01-18 18:33:37 +0100

    Add files via upload

---
+++ AceptaElReto1/src/AceptaElReto/OrganizandoHangares.java
@@ -0,0 +1,19 @@
+package AceptaElReto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author juasan
+ */
+public class OrganizandoHangares {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
