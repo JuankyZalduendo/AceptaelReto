306c4f74b7cb476409e5057b39ee31d89da7bcb7
juan <>
2018-01-22 18:46:15 +0100

    base del programa

--- AceptaElReto1/src/AceptaElReto/OrganizandoHangares.java
+++ AceptaElReto1/src/AceptaElReto/OrganizandoHangares.java
@@ -14,14 +14,48 @@ import java.util.Scanner;
  * @author juasan
  */
 public class OrganizandoHangares {
-    
+    static boolean gameover=false;
     public static void main(String[] args) {
-        Scanner a= new Scanner(System.in);
-        System.out.println("hangares");//temporal
-        int hangares=a.nextInt();
-        while(hangares!=0)
+        
+        while(!gameover)
         {
-            
+            Scanner a= new Scanner(System.in);
+            System.out.println("hangares");//temporal
+            int h=a.nextInt();
+            if(h!=0)
+            {
+                int hangares[]=new int[h];
+                for(int i=0;i<h;i++)
+                {
+                    hangares[i]=a.nextInt();
+                }
+                
+                System.out.println("naves");//temporal
+                int cantidad=a.nextInt();                
+                int naves[]=new int[cantidad];
+                
+                for(int i=0;i<cantidad;i++)
+                {
+                    naves[i]=a.nextInt();
+                }
+                
+                
+                int dato=funcion(hangares, naves);
+                
+                if(dato>=0)
+                    System.out.println("SI");
+                else
+                    System.out.println("NO");
+                
+            }
+            else
+                gameover=true;
         }
+    }
+    
+    public static int funcion(int []hangar, int []naves)
+    {
+        
+        return 0;//temp
     }
 }
